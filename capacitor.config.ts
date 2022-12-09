dd563c6ea5e2f6cac62ddb0a629b147a2b62294f
JoaoPedroBispo <aghata.paulo@etec.sp.gov.br>
2022-12-09 18:19:05 +0000

    Edição do projeto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'formsAp',
+  appName: 'FormsAp',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,21 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   }
+
+
 ];
 @NgModule({
   imports: [
